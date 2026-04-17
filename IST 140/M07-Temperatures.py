e52604f29eca127b71e57ec870d407ce92aa8851
Siyu Wei <weis144024@gmail.com>
2026-04-17 16:22:10 -0400

    Implement temperature generation and analysis
    Fixed function name typo and added temperature generation and analysis functionality.

--- IST 140/M07-Temperatures.py
+++ IST 140/M07-Temperatures.py
@@ -1,4 +1,32 @@
-def genrate_temp():
+import random
+
+def generate_temps():
     temps = []
     for i in range(10):
-        
+        num = random.randint(60, 80)
+        temps.append(num)
+    return temps
+
+def write_data():
+    temperature_list = generate_temps()
+    file = open("daily_temps.txt", "W")
+    for temp in temperature_list:
+        file.write(str(temp) + '\n')
+    file.close()
+
+def analyze_data():
+	total = 0
+	count = 0
+	file = open("daily_temps.txt", "r")
+	for line in file:
+		total += int(line.strip())
+		count += 1
+	file.close()
+	if count > 0:
+		average = total / count
+		print(f"The average daily temperature was {average}°F.")
+
+if __name__ == "__main__":
+	write_data()
+	analyze_data()
+	
