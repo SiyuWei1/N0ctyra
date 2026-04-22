d01b49b5cd89e61e18721c2ecc81698a5120d086
Siyu Wei <weis144024@gmail.com>
2026-04-22 11:30:31 +0800

    Read baby names from file and find common names

--- IST 140/M07-BabyNames.py
+++ IST 140/M07-BabyNames.py
@@ -1,1 +1,16 @@
-# M07-BabyNames
+input_file = open("babynames2010s.txt","r")
+boy_names = []
+girl_names = []
+
+for line in input_file:
+  columns = line.split()
+  boy_name = columns[1]
+  girl_name = columns[3]
+  boy_names.append(boy_name)
+  girl_names.append(girl_name)
+
+input_file.close()
+print("Names that appear on both the boys and girls\"list:")
+for name in boy_names:
+    if name in girl_names:
+        print(name)
